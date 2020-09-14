a3cd7e8d6e0bc0b6d328159c9f0886f078149190
lx <2875467580@qq.com>
2020-09-14 17:22:49 +0800

    修改代码

--- 后端/vueblog/src/main/java/world/lixiang/utils/OssUtils.java
+++ 后端/vueblog/src/main/java/world/lixiang/utils/OssUtils.java
@@ -13,9 +13,9 @@ public class OssUtils {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-beijing.aliyuncs.com";
 // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-        String accessKeyId = "LTAI4GAUGrN9B9uLhkS3BnU1";
-        String accessKeySecret = "XZ7R515pbYHfPutSRsbqxTK2zi5Yjt";
-        String bucketName = "wuqing-zero";
+        String accessKeyId = "";
+        String accessKeySecret = "";
+        String bucketName = "";
 // <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
 
 
